5a76d830dd4bcb7931a49104117ade84358caa4a
David Winn <q@qypea.com>
2024-03-15 08:03:18 -0700

    Initial version, supporting vanilla

---
+++ README.md
@@ -0,0 +1,14 @@
+# space-inside
+I've been enjoying the spaceblock mod a lot recently, but with vanilla recipes and no exploration it felt somewhat hollow at the endgame tiers. This mod strips out one of the core bits of spaceblock into its own mode, which can be layered with other mods to extend that endgame.
+
+Initially my goal is to support vanilla and space exploration, but over time I may add support for other rewrite mods too.
+
+## Features
+1. Greatly reduced miner speeds to help you bootstrap, but to not be generally useful
+2. Resource multiplier recipes for ores, crude oil, water
+
+## Related mods
+Shout out to spaceblock for the inspiration for this mod, and the base of the recipes I created.
+
+## Contributing
+Feel free to make recommendations or raise bugs as github issues or mod portal discussions. PRs or forks are also welcome, but don't assume that I'll invest a lot of time on this. Just a hobby project.
---
+++ data.lua
@@ -0,0 +1,51 @@
+local function dupeOre(ore_item)
+    local dupe = {
+        name = "space-inside-dupe-" .. ore_item.name,
+        enabled = true,
+        type = "recipe",
+        icons = ore_item.icons or { { icon = ore_item.icon } },
+        icon_size = ore_item.icon_size,
+        allow_decomposition = false,
+        order = (ore_item.order and "a3" .. ore_item.order or "a3"),
+        localised_name = ore_item.localised_name or { "entity-name." .. ore_item.name },
+        category = "crafting",
+        energy_required = 5,
+        ingredients = { { type = "item", name = ore_item.name, amount = 5 } },
+        results = { { type = "item", name = ore_item.name, amount = 6 } },
+    }
+
+    if (ore_item.minable and ore_item.minable.required_fluid) then
+        table.insert(dupe.ingredients,
+            { type = "fluid", name = ore_item.minable.required_fluid, amount = ore_item.minable.fluid_amount })
+        dupe.category = "chemistry"
+    end
+    data:extend({ dupe })
+end
+
+local function dupeFluid(fluid_item)
+    local dupe = {
+        name = "space-inside-dupe-" .. fluid_item.name,
+        enabled = true,
+        type = "recipe",
+        icons = fluid_item.icons or { { icon = fluid_item.icon } },
+        icon_size = fluid_item.icon_size,
+        allow_decomposition = false,
+        order = (fluid_item.order and "a3" .. fluid_item.order or "a3"),
+        localised_name = fluid_item.localised_name or { "entity-name." .. fluid_item.name },
+        category = "oil-processing",
+        energy_required = 5,
+        ingredients = { { type = "fluid", name = fluid_item.name, amount = 125 } },
+        results = { { type = "fluid", name = fluid_item.name, amount = 165 } },
+    }
+
+    data:extend({ dupe })
+end
+
+-- Vanilla recipes
+dupeOre(data.raw.resource["iron-ore"])
+dupeOre(data.raw.resource["coal"])
+dupeOre(data.raw.resource["copper-ore"])
+dupeOre(data.raw.resource["stone"])
+dupeOre(data.raw.resource["uranium-ore"])
+dupeFluid(data.raw.resource["crude-oil"])
+dupeFluid(data.raw.fluid["water"])
